2b02b3d8f4371a24bd1b09ee1576bdbf517f443b
ebt35 <eyoub.beraki@hotmail.com>
2025-11-25 13:56:19 +0100

    update

--- 06_fastapi_crud/api.py
+++ 06_fastapi_crud/api.py
@@ -3,6 +3,7 @@ from data_processing import library_data, Book
 
 library = library_data("library.json")
 
+# type hinting
 books: list[Book] = library.books
 
 app = FastAPI()
@@ -10,18 +11,33 @@ app = FastAPI()
 
 @app.get("/books")
 async def read_books():
-    return books 
+    return books
 
-# path parameter 
-@app.get("/books/{id}")
+# path parameter
+@app.get("/book/{id}")
 async def read_book_by_id(id: int):
     return [book for book in books if book.id == id][0]
 
 
 @app.post("/books/create_book")
 async def create_book(book_request: Book):
-    new_book = Book(**book_request)
-    books.append(new_book)
+    new_book = Book.model_validate(book_request)
     
+    # note that the new book is not persisted throughout sessions
+    books.append(new_book)
     return new_book
-    ...
+
+@app.put("/books/update_book")
+async def update_book(updated_book: Book):
+    for i, book in enumerate(books):
+        if book.id == updated_book.id:
+            books[i] = updated_book
+    
+    return updated_book
+
+@app.delete("/books/delete_book/{id}")
+async def update_book(id: int):
+    for i, book in enumerate(books):
+        if book.id == id:
+            del books[i]
+            break
--- 06_fastapi_crud/data_processing.py
+++ 06_fastapi_crud/data_processing.py
@@ -3,23 +3,36 @@ import json
 from pprint import pprint
 from pydantic import BaseModel, Field
 
-
+# convenience functon to read json
 def read_json(filename:str):
     with open(DATA_PATH / filename, 'r') as file:
         data = json.load(file)
         
     return data 
 
+# pydantic models
 class Book(BaseModel):
     id: int
     title: str
     author: str
     year: int = Field(gt = 1000, lt = 2026, description = "Year of when book is published")
     
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 11,
+                "title": "A really cool book",
+                "author": "Coola Coolingen",
+                "year": 2025
+            }
+        }
+    }
+    
 class Library(BaseModel):
     name: str
     books: list[Book]
-    
+
+# validate json intro pydantic models   
 def library_data(filename):
     json_data = read_json(filename)
     return Library(**json_data)
