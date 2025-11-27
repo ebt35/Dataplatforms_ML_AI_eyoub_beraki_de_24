0fd034dba1d0167348f8d55212cfb22d18ed482f
ebt35 <eyoub.beraki@hotmail.com>
2025-11-27 11:27:52 +0100

    database setup

---
+++ 07_b_pydantic_fastapi/utils.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import duckdb
+
+DATA_PATH = Path(__file__).parent / "data"
+
+def query_duckdb(sql_code: str, parameters=None):
+    with duckdb.connect(DATA_PATH / "movies.duckdb") as conn:
+        cursor = conn.execute(sql_code, parameters=parameters)
+        
+        if (
+            sql_code.strip()
+            .lower()
+            .startswith(("select", "from", "desc", "pragma", "with"))
+        ):
+            return cursor.df()
